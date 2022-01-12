d1fb69e74cda0ed0197a457fd1f490ead938ad07
ghost-axe <ghost-axe@outlook.com>
2022-01-12 14:37:33 +0800

    xiugai

--- client/src/libs/getWeb3.js
+++ client/src/libs/getWeb3.js
@@ -1,15 +1,17 @@
 import Web3 from 'web3'
 
-let develop = true
+let develop = false
 
 const getWeb3 = () =>
   new Promise((resolve, reject) => {
     window.addEventListener('load', async () => {
       if (develop) {
+        console.log('...dev')
         const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545')
         const web3 = new Web3(provider)
         resolve(web3)
       } else if (window.ethereum) {
+        console.log(window.ethereum, 'eth...')
         const web3 = new Web3(window.ethereum)
         try {
           await window.ethereum.enable()
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -15,7 +15,7 @@ export default {
     }
   },
   created () {
-    this.init()
+    // this.init()
   },
   methods: {
     async init () {
